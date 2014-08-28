95e2d98d3659761c472f2f1be1190434ef04e27c
Guillermo López :willyaranda <gll@tid.es>
2014-08-28 14:05:16 +0200

    Header updated

--- js/views/header.js
+++ js/views/header.js
@@ -8,51 +8,59 @@ var header = header || {};
 
     var ELEM = document.getElementById('header');
 
+    var previousRoute = '/';
+
     header.controller = function() {};
 
     // Called to update the header from timeline, mentions, dm…
-    header.update = function(previousRoute) {
-        var controller = {};
-        controller.backLink = previousRoute;
-        m.render(ELEM, header.view(controller));
+    header.update = function(back) {
+        previousRoute = back || previousRoute;
+        m.redraw();
     };
 
     header.view = function(controller) {
-        var data = []
+        var data = [];
 
-        if (controller.backLink) {
+        if (previousRoute) {
             var back = m('div#back', [
-                m('a', {href: backLink, config: m.route}, [
+                m('a', {
+                    href: previousRoute, config: m.route
+                }, [
                     m('img', { src: '/img/back.png'})
                 ])
             ]);
             data.push(back);
         }
 
-        data.push(m('div#tl', [
+        var center = [];
+
+        center.push(m('div#tl', [
             m('a', {href: '/timeline', config: m.route}, [
                 m('img', { src: '/img/timeline.png'}),
                 m('span', timeline.getLength())
             ])
         ]));
-        data.push(m('div#mentions', [
+        center.push(m('div#mentions', [
             m('a', {href: '/mentions', config: m.route}, [
                 m('img', { src: '/img/mentions.png'}),
                 m('span', mentions.getLength())
             ])
         ]));
-        data.push(m('div#direct', [
+        center.push(m('div#direct', [
             m('a', {href: '/dms', config: m.route}, [
                 m('img', { src: '/img/direct.png'}),
                 m('span', dms.getLength())
             ])
         ]));
+
+        data.push(m('div#headercenter', center));
+
         data.push(m('div#senddiv', [
             m('a', {
                 href: '/compose',
                 config: m.route
             }, [
-                m('img#send', { src: '/img/send128.png'})
+                m('img#send', { src: '/img/send.png'})
             ])
         ]));
 
